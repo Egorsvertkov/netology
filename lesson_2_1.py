985f26254a6ee70a196fb08127254fd94b3b969f
Egor Svertkov <egor.svertkov@gmail.com>
2017-05-31 13:07:56 +0300

    change metadata in json and code

--- lesson_2_1.py
+++ lesson_2_1.py
@@ -10,9 +10,9 @@ def get_cookbook_from_file():
                                 for i in range(count_of_ingridients):
                                     ingridient = {}
                                     temp_ingridient = cookbook.readline().split(' | ')
-                                    ingridient['ingridient_name'] = temp_ingridient[0].strip()
-                                    ingridient['quantity'] = int(temp_ingridient[1].strip())
-                                    ingridient['measure'] = temp_ingridient[2].strip()
+                                    ingridient['название_ингредиента'] = temp_ingridient[0].strip()
+                                    ingridient['количество'] = int(temp_ingridient[1].strip())
+                                    ingridient['ед_измерения'] = temp_ingridient[2].strip()
                                     cookbook_copy[dish_name].append(ingridient)
                         else:
                             break
@@ -20,24 +20,24 @@ def get_cookbook_from_file():
 
 def create_cookbook_json():
     with open('cookbook.json', 'w') as cookbook_json:
-        json.dump(get_cookbook_from_file(), cookbook_json)
+        json.dump(get_cookbook_from_file(), cookbook_json, indent = 2, ensure_ascii = False)
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
         for ingridient in get_cookbook_from_file()[dish]:
             new_shop_list_item = dict(ingridient)
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            new_shop_list_item['количество'] *= person_count
+            if new_shop_list_item['название_ингредиента'] not in shop_list:
+                shop_list[new_shop_list_item['название_ингредиента']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+                shop_list[new_shop_list_item['название_ингредиента']]['количество'] += new_shop_list_item['количество']
     return shop_list
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
-                                shop_list_item['measure']))
+        print('{} {} {}'.format(shop_list_item['название_ингредиента'], shop_list_item['количество'],
+                                shop_list_item['ед_измерения']))
 
 
 def create_shop_list():
