43422ab2080a530dcc10b3522c09d43bb7458187
Egor Svertkov <egor.svertkov@gmail.com>
2017-05-31 13:06:45 +0300

    change metadata in json and code

--- lesson_2_2.py
+++ lesson_2_2.py
@@ -8,17 +8,17 @@ def get_shop_list_by_dishes(dishes, person_count):
     for dish in dishes:
         for ingridient in cookbook[dish]:
             new_shop_list_item = dict(ingridient)
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            new_shop_list_item['количество'] *= person_count
+            if new_shop_list_item['название_ингредиента'] not in shop_list:
+                shop_list[new_shop_list_item['название_ингредиента']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+                shop_list[new_shop_list_item['название_ингредиента']]['количество'] += new_shop_list_item['количество']
     return shop_list
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
-                                shop_list_item['measure']))
+        print('{} {} {}'.format(shop_list_item['название_ингредиента'], shop_list_item['количество'],
+                                shop_list_item['ед_измерения']))
 
 def create_shop_list():
     person_count = int(input('Введите количество человек: '))
