813b85a658fd5d2a4b3a50cc59f01d71dcd56541
Egor Svertkov <egor.svertkov@gmail.com>
2017-06-05 00:16:50 +0300

    add lesson 2_4 file

--- lesson_2_4.py
+++ lesson_2_4.py
@@ -48,24 +48,24 @@ def get_searching_result(list_of_files, string):
 		search_result = []
 		for file in list_of_files:
 				with open(file) as f:
-					data = f.read()
-					if string in data:
-						search_result.append(file)
-		return(search_result)
+						data = f.read()
+						if string in data:
+								search_result.append(file)
+		return search_result
 
 def print_searching_result(list_of_files):
-	for file in list_of_files:
-		print(file)
-	print('Всего: ', len(list_of_files))
+		for file in list_of_files:
+				print('{}'.format(file))
+		print('Всего: {}'.format(len(list_of_files)))
 
-def search(random_files):
+def search(files):
 		search_string = str(input('Введите строку для поиска (для выхода введите q):'))
 		if search_string != 'q':
-				print_searching_result(get_searching_result(random_files, search_string))
+				print_searching_result(get_searching_result(files, search_string))
 				temp_files = []
-				for file in get_searching_result(random_files, search_string):
+				for file in get_searching_result(files, search_string):
 						temp_files.append(file)
-				return(search(temp_files))
+				return search(temp_files)
 		else:
 				print('Работа программы завершена')
 
